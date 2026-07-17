15d12835a12f24ae924b01f9efa183f7ef26790d
my-websitesolution <satriorich2011@gmail.com>
2026-07-17 14:28:05 +0700

    Update home.js

--- home.js
+++ home.js
@@ -2,6 +2,16 @@ const hamburger = document.querySelector(".hamburger");
 
 const menu = document.querySelector(".menu");
 
+const navLinks = document.querySelectorAll(".navbar a");
+
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("active");
+});
+
+// 2. Fungsi untuk menutup menu (menghapus class 'active')
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active"); // Menghapus class active di tombol hamburger
+    menu.classList.remove("active"); // Menghapus class active di daftar menu agar menutup
+  });
 });
